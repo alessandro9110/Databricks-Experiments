eef784dcf67bf80dae204d7e7785140ed8a649d5
alessandro9110 <alessandro9110>
2025-10-16 22:30:15 +0000

    delete agentic ai

--- Agentic AI/synthia_data_agent/src/configs/variables.py
+++
@@ -1,5 +0,0 @@
-CATALOG_NAME = 'agentic_ai'
-SCHEMA_NAME = 'synthia_data_agent'
-VOLUME_NAME = ''
-
-LLM_ENDPOINT_NAME = 'databricks-llama-4-maverick'
--- Agentic AI/synthia_data_agent/tests/main_test.py
+++
@@ -1,6 +0,0 @@
-from synthia_data_agent import main
-
-
-def test_find_all_taxis():
-    taxis = main.find_all_taxis()
-    assert taxis.count() > 5
